fd0aa85df1754925577db4e3c54aa40c083472c5
manav-sharma69 <manavsharmaskr02@gmail.com>
2026-05-07 19:05:56 +0530

    refactor: Attribution component to include repo link

--- src/components/Attribution/Attribution.jsx
+++ src/components/Attribution/Attribution.jsx
@@ -1,11 +1,12 @@
 import { Box, Flex, Link, Text } from '@radix-ui/themes'
-import { MailIcon } from 'lucide-react'
+import { GithubIcon, MailIcon } from 'lucide-react'
 import { default as NextLink } from 'next/link'
 import ResponsiveContainer from '../RespContainer'
 
 export default function Attribution() {
   const EMAIL = 'mailto:hey@manav.sh'
   const PORTFOLIO = 'https://manav.sh'
+  const GITHUB = 'https://github.com/manav-sharma69/yelpcamp-v2'
   const YEAR = new Date().getFullYear()
   return (
     <Box asChild style={{ background: 'hsl(44, 62%, 70%)' }} mt={'auto'}>
@@ -18,18 +19,20 @@ export default function Attribution() {
             gapY={'3'}
             direction={{ initial: 'column', sm: 'row' }}
           >
-            <Text
-              as="p"
-              align={{ initial: 'center' }}
-              size={{ initial: '2', xs: '3' }}
-            >
-              YelpCamp is created with ❤️ by{' '}
-              <Link asChild color="gray" highContrast>
-                <NextLink target="_blank" href={PORTFOLIO}>
-                  Manav
-                </NextLink>
-              </Link>
-              . |{' '}
+            <Flex gap={'2'}>
+              <Text
+                as="p"
+                align={{ initial: 'center' }}
+                size={{ initial: '2', xs: '3' }}
+              >
+                YelpCamp is created with ❤️ by{' '}
+                <Link asChild color="gray" highContrast>
+                  <NextLink target="_blank" href={PORTFOLIO}>
+                    Manav
+                  </NextLink>
+                </Link>
+                . |{' '}
+              </Text>
               <Link asChild color="gray" highContrast>
                 <Flex
                   display={'inline-flex'}
@@ -37,8 +40,12 @@ export default function Attribution() {
                   align={'center'}
                   asChild
                 >
-                  <NextLink href={EMAIL} target="_blank">
-                    Contact{' '}
+                  <NextLink
+                    href={EMAIL}
+                    target="_blank"
+                    aria-label="Contact Manav"
+                    title="Contact Manav"
+                  >
                     <MailIcon
                       size={'16'}
                       style={{
@@ -47,8 +54,31 @@ export default function Attribution() {
                     />
                   </NextLink>
                 </Flex>
+              </Link>{' '}
+              |{' '}
+              <Link asChild color="gray" highContrast>
+                <Flex
+                  display={'inline-flex'}
+                  gap={'2'}
+                  align={'center'}
+                  asChild
+                >
+                  <NextLink
+                    href={GITHUB}
+                    target="_blank"
+                    aria-label="View GitHub Repo"
+                    title="View GitHub Repo"
+                  >
+                    <GithubIcon
+                      size={'16'}
+                      style={{
+                        display: 'inline'
+                      }}
+                    />
+                  </NextLink>
+                </Flex>
               </Link>
-            </Text>
+            </Flex>
             <Text
               as="p"
               align={{ initial: 'center' }}

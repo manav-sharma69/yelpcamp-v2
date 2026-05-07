f8144e81bdb63e1b845cfaaaff93d73c788449ae
manav-sharma69 <manavsharmaskr02@gmail.com>
2026-05-07 12:26:40 +0530

    refactor: update `Attribution` component & README

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-YELPCAMP
+# YeplCamp V2
 
 Built using Next.js
 
-v3 is intended to overcome its shortcomings
+## Known Vulnerabilities:
 
-TBH, IDK when I'll make v3 🥲
+1. **The auth and session management is weak:** The redirect on logout doesn't work properly. After you have logged out, you'll still stay on the same page and the page becomes unresponsive. The `refresh` function in `SessionProvider` should be fixed.
+
+## Notes
+
+There are a lot of UX related improvements that could be made.
--- src/components/Attribution/Attribution.jsx
+++ src/components/Attribution/Attribution.jsx
@@ -1,8 +1,11 @@
 import { Box, Flex, Link, Text } from '@radix-ui/themes'
+import { MailIcon } from 'lucide-react'
 import { default as NextLink } from 'next/link'
 import ResponsiveContainer from '../RespContainer'
 
 export default function Attribution() {
+  const EMAIL = 'mailto:hey@manav.sh'
+  const PORTFOLIO = 'https://manav.sh'
   const YEAR = new Date().getFullYear()
   return (
     <Box asChild style={{ background: 'hsl(44, 62%, 70%)' }} mt={'auto'}>
@@ -22,11 +25,29 @@ export default function Attribution() {
             >
               YelpCamp is created with ❤️ by{' '}
               <Link asChild color="gray" highContrast>
-                <NextLink target="_blank" href={'https://manav.sh'}>
+                <NextLink target="_blank" href={PORTFOLIO}>
                   Manav
                 </NextLink>
               </Link>
-              .
+              . |{' '}
+              <Link asChild color="gray" highContrast>
+                <Flex
+                  display={'inline-flex'}
+                  gap={'2'}
+                  align={'center'}
+                  asChild
+                >
+                  <NextLink href={EMAIL} target="_blank">
+                    Contact{' '}
+                    <MailIcon
+                      size={'16'}
+                      style={{
+                        display: 'inline'
+                      }}
+                    />
+                  </NextLink>
+                </Flex>
+              </Link>
             </Text>
             <Text
               as="p"
